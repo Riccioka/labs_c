d9f5f4d28a847e382f78f3f4b04830264c851954
kai20u559 <Anna.2002A@yandex.ru>
2021-03-12 22:06:36 +0300

    task 7

---
+++ lab_01_07_02/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+double arcSin(double x, double eps)
+{
+    double s = x;
+    double atmp = x;
+    int n = 0;
+    while(fabs(atmp) > eps){
+        atmp = atmp * ((2 * n + 2) * (2 * n + 1) * x * x * (2 * n + 1)) / (4 * (n + 1) * (n + 1) * (2 * n + 3));
+        s += atmp;
+        n++;
+    }
+    return s;
+}
+
+int main()
+{
+    double x, e, f, s, d1, d2;
+    printf("Введите x: ");
+    scanf("%lf", &x);
+    if ((x > 1) || (x < -1))
+    {
+        printf("Неверный ввод");
+    }
+    printf("Введите e: ");
+    scanf("%lf", &e);
+    f = asin(x);
+    s = arcSin(x, e);
+    d1 = fabs(f-s);
+    d2 = d1 / f;
+    printf("%.12lf %.12lf %.12lf %.12lf", s, f, d1, d2);
+
+
+    return 0;
+}
