dbb78d152c53cdb5a4e11cf2dc9a7f9726268d41
kai20u559 <Anna.2002A@yandex.ru>
2021-03-13 18:49:05 +0300

    test

--- lab_01_02_03/main.c
+++ lab_01_02_03/main.c
@@ -11,7 +11,7 @@ int main()
     printf("Введите угол между ними: ");
     scanf("%lf", &c);
     c = c * M_PI / 180;
-    s = (1.0/2) * a * b * sin(c);
+    s = (1.0 / 2) * a * b * sin(c);
     printf("Sinus - %lf", sin(c));
     printf("Площадь треугольника = %lf", s);
     return 0;
