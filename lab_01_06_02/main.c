5b53ef298e6c126f6782be7006e17fde6d9507b5
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 00:23:22 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int func(double xp, double yp, double xa, double ya, double xb, double yb)
+typedef struct s_point
 {
-    return (xa - xp) * (yb - ya) - (xb - xa) * (ya - yp);
+    double x;
+    double y;
+}               t_point;
+
+double func(t_point p1, t_point p2, t_point p3)
+{
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
 
 int main(void)
 {
-    double xp, yp, xa, ya, xb, yb, xc, yc;
+    t_point p, a, b, c;
     int res;
 
-    if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &xp, &yp, &xa, &ya, &xb, &yb, &xc, &yc) < 8)
+    if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &p.x, &p.y, &a.x, &a.y, &b.x, &b.y, &c.x, &c.y) != 8)
         return 1;
-    if ((func(xp, yp, xa, ya, xb, yb) > 0 && func(xp, yp, xb, yb, xc, yc) > 0 && func(xp, yp, xc, yc, xa, ya) > 0) || (func(xp, yp, xa, ya, xb, yb) < 0 && func(xp, yp, xb, yb, xc, yc) < 0 && func(xp, yp, xc, yc, xa, ya) < 0))
+    if ((func(p, a, b) > 0 && func(p, b, c) > 0 && func(p, c, a) > 0) || (func(p, a, b) < 0 && func(p, b, c) < 0 && func(p, c, a) < 0))
         res = 0;
-    else if (func(xp, yp, xa, ya, xb, yb) *
-        func(xp, yp, xb, yb, xc, yc) *
-        func(xp, yp, xc, yc, xa, ya) == 0)
+    else if (func(p, a, b) * func(p, b, c) * func(p, c, a) == 0)
         res = 1;
     else
         res = 2;
