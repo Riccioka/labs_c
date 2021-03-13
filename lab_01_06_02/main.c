403d3c87e8b732ece0cc748ca9c4c8175247adf2
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 00:35:35 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 
-typedef struct s_point
-{
-    double x;
-    double y;
-}               t_point;
-
-double func(t_point p1, t_point p2, t_point p3)
-{
-    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-}
-
 int main(void)
 {
-    t_point p, a, b, c;
-    int res;
-    printf("%d", res);
     return 0;
 }
