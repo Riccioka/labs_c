ba6f13691ebdfde858e4608bc19b192377781e1c
kai20u559 <Anna.2002A@yandex.ru>
2021-03-13 23:48:06 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int func(float xp, float yp, float xa, float ya, float xb, float yb)
+int func(double xp, double yp, double xa, double ya, double xb, double yb)
 {
     return (xa - xp) * (yb - ya) - (xb - xa) * (ya - yp);
 }
-
 
 int main(void)
 {
     double xp, yp, xa, ya, xb, yb, xc, yc;
     int res;
+
     if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &xp, &yp, &xa, &ya, &xb, &yb, &xc, &yc) < 8)
         return 1;
     if ((func(xp, yp, xa, ya, xb, yb) > 0 && func(xp, yp, xb, yb, xc, yc) > 0 && func(xp, yp, xc, yc, xa, ya) > 0) || (func(xp, yp, xa, ya, xb, yb) < 0 && func(xp, yp, xb, yb, xc, yc) < 0 && func(xp, yp, xc, yc, xa, ya) < 0))
