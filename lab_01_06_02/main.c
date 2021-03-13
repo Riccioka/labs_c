dab61b7ef685e8d27bb76bba6236c12decf3b61b
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 00:31:20 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -15,15 +15,6 @@ int main(void)
 {
     t_point p, a, b, c;
     int res;
-
-    if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &p.x, &p.y, &a.x, &a.y, &b.x, &b.y, &c.x, &c.y) != 8)
-        return 1;
-    if ((func(p, a, b) > 0 && func(p, b, c) > 0 && func(p, c, a) > 0) || (func(p, a, b) < 0 && func(p, b, c) < 0 && func(p, c, a) < 0))
-        res = 0;
-    else if (func(p, a, b) * func(p, b, c) * func(p, c, a) == 0)
-        res = 1;
-    else
-        res = 2;
     printf("%d", res);
     return 0;
 }
