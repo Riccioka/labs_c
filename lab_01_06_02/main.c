42a35bb7ccd3b76de849c61f8ea71631bbe4b0d2
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 02:51:58 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int func(int xp, int yp, int xa, int ya, int xb, int yb)
+double func(double xp, double yp, double xa, double ya, double xb, double yb)
 {
     return (xa - xp) * (yb - ya) - (xb - xa) * (ya - yp);
 }
 
 int main()
 {
-    int xp, yp, xa, ya, xb, yb, xc, yc, res;
-    if (scanf("%d %d %d %d %d %d %d %d", &xp, &yp, &xa, &ya, &xb, &yb, &xc, &yc) < 8)
+    double xp, yp, xa, ya, xb, yb, xc, yc;
+    int res;
+    if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &xp, &yp, &xa, &ya, &xb, &yb, &xc, &yc) < 8.0)
         return -1;
-    if (((func(xp, yp, xa, ya, xb, yb) > 0) && (func(xp, yp, xb, yb, xc, yc) > 0) && (func(xp, yp, xc, yc, xa, ya) > 0)) || ((func(xp, yp, xa, ya, xb, yb) < 0) && (func(xp, yp, xb, yb, xc, yc) < 0) && (func(xp, yp, xc, yc, xa, ya) < 0)))
+    if (((func(xp, yp, xa, ya, xb, yb) > 0.0) && (func(xp, yp, xb, yb, xc, yc) > 0.0) && (func(xp, yp, xc, yc, xa, ya) > 0.0)) || ((func(xp, yp, xa, ya, xb, yb) < 0.0) && (func(xp, yp, xb, yb, xc, yc) < 0.0) && (func(xp, yp, xc, yc, xa, ya) < 0.0)))
         res = 0;
-    else if ((func(xp, yp, xa, ya, xb, yb) * func(xp, yp, xb, yb, xc, yc) * func(xp, yp, xc, yc, xa, ya)) == 0)
+    else if ((func(xp, yp, xa, ya, xb, yb) == 0.0) || (func(xp, yp, xb, yb, xc, yc) == 0.0) || (func(xp, yp, xc, yc, xa, ya) == 0.0))
         res = 1;
     else
         res = 2;
