39b59921598909a98bfa5a3351f9c3fe67ebd40b
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 01:38:43 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+int func(double xp, double yp, double xa, double ya, double xb, double yb)
+{
+    return (xa - xp) * (yb - ya) - (xb - xa) * (ya - yp);
+}
+
 int main(void)
 {
     double xp, yp, xa, ya, xb, yb, xc, yc;
     int res;
 
+    if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &xp, &yp, &xa, &ya, &xb, &yb, &xc, &yc) < 8.0)
+        return 1;
+    if ((func(xp, yp, xa, ya, xb, yb) > 0.0 && func(xp, yp, xb, yb, xc, yc) > 0.0 && func(xp, yp, xc, yc, xa, ya) > 0.0) || (func(xp, yp, xa, ya, xb, yb) < 0.0 && func(xp, yp, xb, yb, xc, yc) < 0.0 && func(xp, yp, xc, yc, xa, ya) < 0.0))
+        res = 0;
+    else if (func(xp, yp, xa, ya, xb, yb) *
+        func(xp, yp, xb, yb, xc, yc) *
+        func(xp, yp, xc, yc, xa, ya) == 0.0)
+        res = 1;
+    else
+        res = 2;
     printf("%d", res);
     return 0;
 }
