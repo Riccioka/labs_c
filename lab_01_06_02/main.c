809c63545f4ec1b2afa9e863ba6ee0a0bd831d87
kai20u559 <Anna.2002A@yandex.ru>
2021-03-13 19:09:54 +0300

    task 6

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -2,52 +2,41 @@
 
 int func(float xp, float yp, float xa, float ya, float xb, float yb)
 {
-    return  (xa - xp) * (yb-ya) - (xb - xa) * (ya - yp);
+    return  ((xa - xp) * (yb-ya) - (xb - xa) * (ya - yp));
 }
 
 int main()
 {
     double xp, yp, xa, ya, xb, yb, xc, yc;
-    printf("Введите абсциссу P: ");
     int r = scanf("%lf", &xp);
     if (r == 0)
         return 1;
-    printf("Введите ординату P: ");
     r = scanf("%lf", &yp);
     if (r == 0)
         return 1;
-    printf("Введите абсциссу А: ");
     r = scanf("%lf", &xa);
     if (r == 0)
         return 1;
-    printf("Введите ординату А: ");
     r = scanf("%lf", &ya);
     if (r == 0)
         return 1;
-    printf("Введите абсциссу B: ");
     r = scanf("%lf", &xb);
     if (r == 0)
         return 1;
-    printf("Введите ординату B: ");
     r = scanf("%lf", &yb);
     if (r == 0)
         return 1;
-    printf("Введите абсциссу C: ");
     r = scanf("%lf", &xc);
     if (r == 0)
         return 1;
-    printf("Введите ординату C: ");
     r = scanf("%lf", &yc);
     if (r == 0)
         return 1;
-    if ((func(xp, yp,xa, ya, xb, yb) > 0) &&
-        (func(xp, yp,xb, yb, xc, yc) > 0) && (func(xp, yp,xc, yc, xa, ya) > 0))
+    if ((func(xp, yp, xa, ya, xb, yb) > 0) && (func(xp, yp, xb, yb, xc, yc) > 0) && (func(xp, yp, xc, yc, xa, ya) > 0))
         printf("0");
-    else if ((func(xp, yp,xa, ya, xb, yb) < 0) &&
-    (func(xp, yp,xb, yb, xc, yc) < 0) && (func(xp, yp,xc, yc, xa, ya) < 0))
+    else if ((func(xp, yp, xa, ya, xb, yb) < 0) && (func(xp, yp, xb, yb, xc, yc) < 0) && (func(xp, yp, xc, yc, xa, ya) < 0))
         printf("0");
-    else if ((func(xp, yp,xa, ya, xb, yb)) *
-             (func(xp, yp,xb, yb, xc, yc)) * (func(xp, yp,xc, yc, xa, ya)) == 0)
+    else if ((func(xp, yp, xa, ya, xb, yb)) * (func(xp, yp, xb, yb, xc, yc)) * (func(xp, yp, xc, yc, xa, ya)) == 0)
         printf("1");
     else
         printf("2");
