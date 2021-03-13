b9acdee2e074f6b610be19fb6d96c5d50bb23e15
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 00:56:58 +0300

    test

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+typedef struct s_point
+{
+    double x;
+    double y;
+}               t_point;
+
 int main(void)
 {
+    t_point p, a, b, c;
+    int res;
+
+    printf("%d", res);
     return 0;
 }
