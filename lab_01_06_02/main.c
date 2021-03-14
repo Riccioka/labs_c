7c19fb1fbb10f94651cb160116fa55c6f6208605
kai20u559 <Anna.2002A@yandex.ru>
2021-03-14 08:46:18 +0300

    task 6

--- lab_01_06_02/main.c
+++ lab_01_06_02/main.c
@@ -12,6 +12,8 @@ int main()
     int res;
     if (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &xp, &yp, &xa, &ya, &xb, &yb, &xc, &yc) < 8.0)
         return -1;
+    if ((fabs((xa - xc) * (yb - yc) - (xb - xc) * (ya - yc)) - 0) < 0.0000001)
+        return -1;
     if (((func(xp, yp, xa, ya, xb, yb) > 0.0) && (func(xp, yp, xb, yb, xc, yc) > 0.0) && (func(xp, yp, xc, yc, xa, ya) > 0.0)) || ((func(xp, yp, xa, ya, xb, yb) < 0.0) && (func(xp, yp, xb, yb, xc, yc) < 0.0) && (func(xp, yp, xc, yc, xa, ya) < 0.0)))
         res = 0;
     else if ((fabs(func(xp, yp, xa, ya, xb, yb) - 0) < 0.0000001) || (fabs(func(xp, yp, xb, yb, xc, yc) - 0) < 0.0000001) || (fabs(func(xp, yp, xc, yc, xa, ya) - 0) < 0.0000001))
