528f31649456fc192e5fb3e22636d1cd87d470d5
kai20u559 <Anna.2002A@yandex.ru>
2021-03-16 16:31:23 +0300

    task 1

--- lab_02_01_00/main.c
+++ lab_02_01_00/main.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-int count(long *a, int n)
+long long sum(long *a, int n)
 {
-    int c = 0;
-    int i;
-    for (i = 0; i < n; i++)
-        if (a[i] % 2 == 0)
-            c += 1;
-    return c;
-}
-
-long sum(long *a, int n)
-{
-    long s = 0;
+    long long s = 0;
     int i;
     for (i = 0; i < n; i++)
         if (a[i] % 2 == 0)
@@ -22,26 +12,20 @@ long sum(long *a, int n)
 
 int main()
 {
-    int i, n, c;
+    int i, n;
     long a[10];
     if (scanf("%d", &n) == 0)
     {
         printf("invalid size value");
     }
-    c = count(a, n);
-    if (c == 0)
-        return 1;
-    else
+    for (i = 0; i < n; i++)
     {
-        for (i = 0; i < n; i++)
+        if (scanf("%ld", &a[i]) == 0)
         {
-            if (scanf("%ld", &a[i]) == 0)
-            {
-                printf("invalid element value");
-                return 1;
-            }
+            printf("invalid element value");
+            return 1;
         }
-        printf("%ld", sum(a, n));
-        return 0;
     }
+    printf("%lld", sum(a, n));
+    return 0;
 }
