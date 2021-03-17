cf4024ff9d9f21e506bc497c0c3143f3f13a1239
kai20u559 <Anna.2002A@yandex.ru>
2021-03-17 12:42:57 +0300

    task 1

--- lab_02_01_00/main.c
+++ lab_02_01_00/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 long long sum(long long a, long long *s)
 {
@@ -25,6 +26,11 @@ int main()
     for (i = 0; i < n; i++)
     {
         if (scanf("%lld", &a[i]) <= 0)
+        {
+            printf("invalid element value");
+            return 1;
+        }
+        if ((abs(a[i]) + s) > (pow(2, 63) - 1))
         {
             printf("invalid element value");
             return 1;
