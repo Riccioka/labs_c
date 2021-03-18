39e1b6157318a937f7b2506a1389da26f9efdd59
kai20u559 <Anna.2002A@yandex.ru>
2021-03-18 14:53:54 +0300

    task 2

--- lab_02_02_01/main.c
+++ lab_02_02_01/main.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
+#include <math.h>
 
-int isprime(long long n) // prime
+int read_arr(long long *arr, int size)
 {
-    int i = 2;
-
-    while (i * i <= n && i <= 46340 && n % i != 0)
-        ++i;
-    return (i == 2 || n % i);
-}
-
-int main()
-{
-    int i, j;
-    long long n;
-    long long a[10];
-    long long b[10];
-
-    if (scanf("%lld", &n) <= 0 || n > 10 || n < 0)
+    for (int i = 0; i < size; i++)
     {
-        printf("invalid size value");
-        return 1;
-    }
-    for (i = 0; i < n; i++)     //ввод массива а
-    {
-        if (scanf("%lld", &a[i]) <= 0)
+        if (scanf("%lld", arr + i) <= 0)
         {
             printf("invalid element value");
             return 1;
         }
     }
-    j = 0;
-    for (i = 0; i < n; i++)
+    return 0;
+}
+
+void print_arr(long long *arr, int size)
+{
+    for (int i = 0; i < size; ++i)
+        printf("%lld ", arr[i]);
+}
+
+int main()
+{
+    int j = 0;
+    long long n = 0;
+    long long a[10];
+    long long b[10];
+
+    if (scanf("%lld", &n) <= 0 || n > 10 || n < 0 || read_arr(&a[0], n))
+        return 1;
+    for (int i = 0; i < n; ++i)
     {
-        if (isprime(a[i]))
-            b[j++] = a[i];
+        if (a[i] >= 0)
+        {
+            int temp = (int)sqrt(a[i]);
+            if (temp * temp == a[i])
+                continue;
+        }
+        b[j++] = a[i];
     }
-    for (i = 0; i < j; i++)
-        printf("%lld ", b[i]);
+    print_arr(&b[0], j);
     return 0;
 }
