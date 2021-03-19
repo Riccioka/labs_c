418300f6a64340bd7b1d27ecd3d33b26a42a95bb
kai20u559 <Anna.2002A@yandex.ru>
2021-03-19 19:43:27 +0300

    task 6

--- lab_02_02_01/main.c
+++ lab_02_02_01/main.c
@@ -2,11 +2,15 @@
 
 int isprime(long long n)
 {
-    int i = 2;
-
-    while (i * i <= n && i <= 46340 && n % i != 0)
-        ++i;
-    return (i == 2 || n % i);
+    if (n > 1)
+    {
+        for (int i = 2; i * i < n; i++)
+            if (n % i == 0)
+                return 1;
+        return 0;
+    }
+    else
+        return 1;
 }
 
 int main()
@@ -32,7 +36,7 @@ int main()
     j = 0;
     for (i = 0; i < n; i++)
     {
-        if (isprime(a[i]))
+        if (isprime(a[i]) == 0)
             b[j++] = a[i];
     }
     for (i = 0; i < j; i++)
