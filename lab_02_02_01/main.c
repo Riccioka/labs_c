9112636f8dd9ba99aa935b0f5a3518d3c50f33ca
kai20u559 <Anna.2002A@yandex.ru>
2021-03-21 22:26:39 +0300

    task 2

--- lab_02_02_01/main.c
+++ lab_02_02_01/main.c
@@ -4,7 +4,7 @@ int isprime(int n)
 {
     if (n > 1)
     {
-        for (int i = 2; i < n; i++)
+        for (int i = 2; i * i < n; i++)
 //        for (int i = 2; i < n; i++)
             if (n % i == 0)
                 return 1;
