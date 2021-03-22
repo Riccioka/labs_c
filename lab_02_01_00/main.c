47840406cecf74563b55f0d188b244f821f16545
kai20u559 <Anna.2002A@yandex.ru>
2021-03-22 16:35:31 +0300

    check task 1

--- lab_02_01_00/main.c
+++ lab_02_01_00/main.c
@@ -15,7 +15,7 @@ int main()
     int n, i;
     int a[10];
     int s = 0;
-//    int k = 0;
+    int k = 0;
     if (scanf("%d", &n) != 1 || n > 10 || n <= 0)
     {
         printf("invalid size value");
@@ -33,16 +33,16 @@ int main()
             printf("invalid element value");
             return 1;
         }
-//        int s1 = s;
+        int s1 = s;
         sum(a[i], &s);
-//        if (s1 != s)
-//            k = 1;
+        if ((s1 != s) && (a[i] != 0))
+            k = 1;
     }
-//    if (k == 0)
-//    {
-//        printf("no one");
-//        return 1;
-//    }
+    if (k == 0)
+    {
+        printf("no one");
+        return 1;
+    }
     printf("%d ", s);
     return 0;
 }
