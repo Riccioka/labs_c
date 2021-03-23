f570349f19e207340f50104fd6c7918720ee1d3e
kai20u559 <Anna.2002A@yandex.ru>
2021-03-23 13:23:30 +0300

    check task 2

--- lab_02_02_01/main.c
+++ lab_02_02_01/main.c
@@ -2,16 +2,10 @@
 
 int isprime(int n)
 {
-    if (n > 1)
-    {
-//        for (int i = 2; i * i < n; i++)
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
-                return 1;
-        return 0;
-    }
-    else
-        return 1;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return 0;
+    return (n > 1);
 }
 
 int main()
@@ -38,7 +32,7 @@ int main()
     j = 0;
     for (i = 0; i < n; i++)
     {
-        if (isprime(a[i]) == 0)
+        if (isprime(a[i]))
         {
             k = 1;
             b[j++] = a[i];
@@ -53,3 +47,4 @@ int main()
         printf("%d ", b[i]);
     return 0;
 }
+
