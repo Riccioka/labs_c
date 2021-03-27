fd6f5ad6cf2c1107b6aa8d1e801c5696e5cc57f4
kai20u559 <Anna.2002A@yandex.ru>
2021-03-27 09:32:28 +0300

    task 7 1

--- lab_01_07_02/main.c
+++ lab_01_07_02/main.c
@@ -6,16 +6,17 @@ double arcsinus(double x, double eps)
     double s = x;
     double atmp = x;
     int n = 0;
+
     while (fabs(atmp) > eps)
     {
-        atmp = atmp * ((2 * n + 2) * (2 * n + 1) * x * x * (2 * n + 1)) / (4 * (n + 1) * (n + 1) * (2 * n + 3));
+        atmp *= atmp * atmp * (n - 1) * (n - 1) / ((n + 2) * (n + 1));
         s += atmp;
-        n++;
+        n += 2;
     }
     return s;
 }
 
-int main()
+int main(void)
 {
     double x, e, f, s, d1, d2;
     if (scanf("%lf", &x) == 0 || scanf("%lf", &e) == 0 || x > 1 || x < -1)
