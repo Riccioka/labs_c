01c3e57d27d1cd03b875b856f3662e5b66c1eaae
kai20u559 <Anna.2002A@yandex.ru>
2021-04-04 12:49:59 +0300

    task 1

--- lab_03_01_03/main.c
+++ lab_03_01_03/main.c
@@ -19,7 +19,7 @@ void print_arr(int *arr, int size)
         printf("%d ", arr[i]);
 }
 
-//int search(a)
+//int first_diff(a)
 //    int f = 0;
 //    for (j = 0; j < m - 1; j++)
 //        if (f == 0)
@@ -35,11 +35,12 @@ int main()
 {
     int n = 0;
     int m = 0;
+    int ind = 0;
     int a[10][10];
     int b[100];
     int i, j, f, k;
 
-    if (scanf("%d", &n) <= 0 || n > 10 || n < 0 || scanf("%d", &m) <= 0 || m > 10 || m < 0 || read_arr(a, n, m))
+    if (scanf("%d", &n) <= 0 || n > 10 || n < 1 || scanf("%d", &m) <= 0 || m > 10 || m < 2 || read_arr(a, n, m))
         return 1;
 
     for (i = 0; i < n; i++)
@@ -53,19 +54,36 @@ int main()
                 if (a[i][j] < a[i][j + 1])
                     f = 1;
             }
-        k = 0;
-        for (j = 0; j < m - 1; j++)
-        {
-            if ((a[i][j] >= a[i][j + 1] && f == 2) || (a[i][j] <= a[i][j + 1] && f == 1))
-                b[i * j + j] = a[i][j];
             else
-                k = 1;
+                break;
+        if (f != 0)
+        {
+            k = 0;
+            for (j = 0; j < m - 1; j++)
+            {
+                if ((a[i][j] >= a[i][j + 1] && f == 2) || (a[i][j] <= a[i][j + 1] && f == 1))
+                {
+                    b[ind] = a[i][j];
+                    ind++;
+                }
+                else
+                {
+                    k = 1;
+                    break;
+                }
+            }
+            if (k == 0)
+            {
+                b[ind] = a[i][m - 1];
+                ind++;
+            }
         }
-        if (k == 0)
-            b[i * m + m - 1] = a[i][m - 1];
-        if (f == 0)
+        else
             for (j = 0; j < m; j++)
-                b[i * j + j] = a[i][j];
+            {
+                b[ind] = a[i][j];
+                ind++;
+            }
     }
     print_arr(&b[0], j);
     return 0;
