3e77f2397f5d2eebcc5b817648e74881d3c1caba
kai20u559 <Anna.2002A@yandex.ru>
2021-04-17 10:04:14 +0300

    task 4

--- lab_03_04_00/main.c
+++ lab_03_04_00/main.c
@@ -29,7 +29,7 @@ int main()
     int a[10][10];
 
     if (scanf("%d", &n) <= 0 || n > 10 || n < 2 ||
-        scanf("%d", &m) <= 0 || m > 10 || m < 2 || n == m ||
+        scanf("%d", &m) <= 0 || m > 10 || m < 2 || n != m ||
         read_mat(a, n, m))
         return -1;
 
