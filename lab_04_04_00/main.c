807b412a33df8a62dce9081fdd3b7991216474f4
kai20u559 <Anna.2002A@yandex.ru>
2021-05-03 13:28:43 +0300

    task 4 was added

---
+++ lab_04_04_00/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+char *skip_spaces(char *str)
+{
+    while (*str == ' ' || *str == '\t')
+        ++str;
+    return str;
+}
+
+int my_atoi_mod(char **str)
+{
+    int c = 0;
+    int count = 0;
+
+    while (isdigit(**str))
+    {
+        c = c * 10 + (**str - '0');
+        if (c > 255)
+            return -1;
+        (*str)++;
+        count++;
+    }
+    return (count) ? c : -1;
+}
+
+int is_valid_ip(char *str)
+{
+    char *walker = str;
+    int c = 0;
+
+    walker = skip_spaces(walker);
+
+    c = my_atoi_mod(&walker);
+    if (c < 0 || *walker != '.')
+        return 0;
+
+    walker++;
+    c = my_atoi_mod(&walker);
+    if (c < 0 || *walker != '.')
+        return 0;
+
+    walker++;
+    c = my_atoi_mod(&walker);
+    if (c < 0 || *walker != '.')
+        return 0;
+
+    walker++;
+    c = my_atoi_mod(&walker);
+    return (c >= 0 && *skip_spaces(walker) == 0);
+}
+
+int main(void)
+{
+    char str[256];
+    int c = 0;
+
+    if (scanf("%256[^\n]%*c", str) != 1)
+        return 1;
+
+    if ((c = is_valid_ip(str)))
+        printf("YES");
+    else
+        printf("NO");
+
+    return !c;
+}
