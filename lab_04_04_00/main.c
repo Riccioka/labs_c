23e49cb7c1fd3f65e94aff8d3c97fdce4dfefa2d
kai20u559 <Anna.2002A@yandex.ru>
2021-05-08 18:31:49 +0300

    try 3

--- lab_04_04_00/main.c
+++ lab_04_04_00/main.c
@@ -23,15 +23,19 @@ int my_atoi_mod(char **str)
 {
     int c = 0;
     int count = 0;
-
+    int count_null = 0;
     while (isdigit(**str))
     {
+        if (**str == '0')
+            count_null++;
         c = c * 10 + (**str - '0');
         if (c > 255)
             return -1;
         (*str)++;
         count++;
     }
+    if (count_null > 3)
+        return -1;
     return (count) ? c : -1;
 }
 
