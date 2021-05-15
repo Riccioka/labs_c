80a50cb0368d65a5922ac9d635283c47f7d23fca
kai20u559 <Anna.2002A@yandex.ru>
2021-05-15 19:51:20 +0300

    task1 was added

---
+++ lab_05_01_02/process.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define OK 0
+#define ERROR_OPEN -2
+#define ERROR_FILE -3
+
+int process(FILE *f)
+{
+    int count = 0;
+    int num = 0;
+    int res = 0;
+    int max = INT_MIN;
+    int max_ind = -1;
+
+    if (f == NULL || feof(f))
+        return ERROR_OPEN;
+    while ((res = fscanf(f, "%d", &num)) == 1)
+    {
+        count++;
+        if (num > max)
+        {
+            max = num;
+            max_ind = count;
+        }
+    }
+    if (res == EOF)
+        return max_ind;
+    return ERROR_FILE;
+}
---
+++ lab_05_01_02/process.h
@@ -0,0 +1,7 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+#include <stdio.h>
+
+int process(FILE *f);
+
+#endif // PROCESS_H
