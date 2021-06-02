75a5aec2ddabdad76da25b0daefe98ee269e39a6
kai20u559 <Anna.2002A@yandex.ru>
2021-06-02 21:11:43 +0300

    task 3

--- lab_05_03/main.c
+++ lab_05_03/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
 #include "get_number_by_pos.h"
 #include "put_number_by_pos.h"
 #include "c.h"
@@ -10,6 +12,7 @@
 
 int main(int argc, char **argv)
 {
+    srand(time(NULL));
     if (argc > 4 || argc < 3 || strlen(argv[1]) != 1)
         return -1;
     if (argc == 4 && argv[1][0] == 'c')
--- lab_05_03/p.c
+++ lab_05_03/p.c
@@ -5,17 +5,17 @@
 int p(char *fname)
 {
     FILE *fp = fopen(fname, "rb");
-//    size_t size = 0;
+    size_t size = 0;
     int n = 0;
 
     if (!fp)
         return -1;
 
-//    fseek(fp, 0, SEEK_END);
-//    size = ftell(fp);
-//    fseek(fp, 0, SEEK_SET);
-//	if (size % 4)
-//		return -1;
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    if (size % 4)
+        return -1;
     while (fread(&n, sizeof(n), 1, fp) == 1)
         printf("%d\n", n);
     fclose(fp);
