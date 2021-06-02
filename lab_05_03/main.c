d8aeec7547afe3146917f605b2f874c8ff234a6a
kai20u559 <Anna.2002A@yandex.ru>
2021-06-02 23:15:19 +0300

    task 3

--- lab_05_03/main.c
+++ lab_05_03/main.c
@@ -2,110 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool failflag = false;
-
-int get_number_by_pos(FILE *fp, int pos)
-{
-    int n = 0;
-    fseek(fp, pos * sizeof(n), SEEK_SET);
-    if (!feof(fp)) failflag = (fread(&n, sizeof(n), 1, fp) == 0); else failflag = true;
-    return n;
-}
-
-int put_number_by_pos(FILE *fp, int pos, int n)
-{
-    fseek(fp, pos * sizeof(n), SEEK_SET);
-    return fwrite(&n, sizeof(n), 1, fp);
-}
-
-int c(char *fname, int num)
-{
-    FILE *fp = fopen(fname, "wb");
-    int n = 0;
-
-    if (!fp)
-        return -1;
-    for (int i = 0; i < num; ++i)
-    {
-        n = rand();
-        printf("%d ", n);
-        if (put_number_by_pos(fp, i, n) != 1)
-            return -1;
-    }
-    fclose(fp);
-    return 0;
-}
-
-
-int p(char *fname)
-{
-    FILE *fp = fopen(fname, "rb");
-    int n = 0;
-
-    if (!fp)
-        return -1;
-    int i = 0;
-    while (!feof(fp))
-    {
-        failflag = false;
-        n = get_number_by_pos(fp, i);
-        if (!failflag) printf("%d ", n);
-        i++;
-    }
-    fclose(fp);
-    return 0;
-}
-
-
-int s(char *fname)
-{
-    FILE *fp = fopen(fname, "r+b");
-    if (!fp)
-        return -1;
-    int n, m;
-    int i = 0;
-    int j = 0;
-    while (!feof(fp))
-    {
-        failflag = false;
-        n = get_number_by_pos(fp, i);
-        if (!failflag)
-        {
-            j = i;
-            while (!feof(fp))
-            {
-                j++;
-                failflag = false;
-                m = get_number_by_pos(fp, j);
-                if (!failflag)
-                {
-                    if (m < n)
-                    {
-                        put_number_by_pos(fp, j, n);
-                        put_number_by_pos(fp, i, m);
-                        break;
-                    }
-                }
-                else break;
-            }
-            i++;
-        }
-        else break;
-    }
-    fclose(fp);
-    return 0;
-}
+#include "get_number_by_pos.h"
+#include "put_number_by_pos.h"
+#include "c.h"
+#include "p.h"
+#include "s.h"
 
 
 int main(int argc, char **argv)
 {
-    if (argc < 3 || strlen(argv[1]) != 1)
+    if (argc > 4 || argc < 3 || strlen(argv[1]) != 1)
         return -1;
-    if (argv[1][0] == 'c')
-        return c(argv[argc - 1], atoi(argv[2]));
+    if (argc == 4 && argv[1][0] == 'c')
+        return 0;
     else if (argv[1][0] == 'p')
         return p(argv[argc - 1]);
     else if (argv[1][0] == 's')
-        return s(argv[argc - 1]);
+        return 0;
     return -1;
 }
