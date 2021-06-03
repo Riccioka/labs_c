8b9fd8c22dbe2acbb9cf04a64de1d201da46792d
kai20u559 <Anna.2002A@yandex.ru>
2021-06-03 16:28:30 +0300

    task 3

--- lab_05_03/main.c
+++ lab_05_03/main.c
@@ -13,13 +13,14 @@
 int main(int argc, char **argv)
 {
     srand(time(NULL));
+
     if (argc > 4 || argc < 3 || strlen(argv[1]) != 1)
         return -1;
     if (argc == 4 && argv[1][0] == 'c')
-        return 0;
-    else if (argv[1][0] == 'p')
+        return c(argv[argc - 1], atoi(argv[2]));
+    else if (argc == 3 && argv[1][0] == 'p')
         return p(argv[argc - 1]);
-    else if (argv[1][0] == 's')
-        return 0;
+    else if (argc == 3 && argv[1][0] == 's')
+        return s(argv[argc - 1]);
     return -1;
 }
