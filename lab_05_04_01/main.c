dfe4969ac1ad3d43e21afbd3ced56912da7f03c8
kai20u559 <Anna.2002A@yandex.ru>
2021-06-05 20:42:07 +0300

    task 4 sort

--- lab_05_04_01/main.c
+++ lab_05_04_01/main.c
@@ -9,9 +9,9 @@ int main(int argc, char **argv)
     int ret = 53;
 //    if (argc == 5 && !strcmp(argv[1], "fb"))
 //        ret = print(argv[2], argv[3], argv[4]);
-//    else if (argc == 3 && !strcmp(argv[1], "sb"))
-//        ret = sort(argv[2]);
-    if (argc == 3 && !strcmp(argv[1], "db"))
-        ret = del(argv[2]);
+    if (argc == 3 && !strcmp(argv[1], "sb"))
+        ret = sort(argv[2]);
+//    else if (argc == 3 && !strcmp(argv[1], "db"))
+//        ret = del(argv[2]);
     return ret;
 }
