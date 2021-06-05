94b92ea16a870e6429ae910b670931e50ec2ce57
kai20u559 <Anna.2002A@yandex.ru>
2021-06-05 19:17:09 +0300

    task 4

--- lab_05_04_01/main.c
+++ lab_05_04_01/main.c
@@ -7,12 +7,13 @@ int main(int argc, char **argv)
 {
     int ret = 53;
     if (argc == 5 && !strcmp(argv[1], "fb"))
-        ret = print(argv[2], argv[3], argv[4]);
+        ret = 53;
+//        ret = print(argv[2], argv[3], argv[4]);
     else if (argc == 3 && !strcmp(argv[1], "sb"))
         ret = 53;
 //        ret = sort(&argv[2]);
     else if (argc == 3 && !strcmp(argv[1], "db"))
-//        ret = del(&argv[2]);
-        ret = 53;
+        ret = del(&argv[2]);
+//        ret = 53;
     return ret;
 }
