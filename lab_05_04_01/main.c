0e0c2d979f4077b448db89767d2806cae5aafd02
kai20u559 <Anna.2002A@yandex.ru>
2021-06-05 20:43:59 +0300

    task 4

--- lab_05_04_01/main.c
+++ lab_05_04_01/main.c
@@ -5,13 +5,12 @@
 
 int main(int argc, char **argv)
 {
-// print_students(fopen("sss", "rb")); return 0;
     int ret = 53;
-//    if (argc == 5 && !strcmp(argv[1], "fb"))
-//        ret = print(argv[2], argv[3], argv[4]);
-    if (argc == 3 && !strcmp(argv[1], "sb"))
+    if (argc == 5 && !strcmp(argv[1], "fb"))
+        ret = print(argv[2], argv[3], argv[4]);
+    else if (argc == 3 && !strcmp(argv[1], "sb"))
         ret = sort(argv[2]);
-//    else if (argc == 3 && !strcmp(argv[1], "db"))
-//        ret = del(argv[2]);
+    else if (argc == 3 && !strcmp(argv[1], "db"))
+        ret = del(argv[2]);
     return ret;
 }
